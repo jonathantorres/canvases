bbc34450b36badccb57c7bade6d814c39da764fb
Jonathan Torres <jonathantorres41@gmail.com>
2013-03-19 00:01:32 -0400

    Edge boundaries-Removal, and regeneration

---
+++ peters_animation_book/ch6/Ball.js
@@ -0,0 +1,33 @@
+(function(window) {
+
+	function Ball(radius, color) {
+		this.initialize(radius, color);
+	}
+
+	Ball.prototype = new Container();
+
+	Ball.prototype.radius;
+	Ball.prototype.color;
+	Ball.prototype.vx = 0;
+	Ball.prototype.vy = 0;
+
+	Ball.prototype.Container_initialize = Ball.prototype.initialize;
+
+	Ball.prototype.initialize = function(radius, color) {
+		this.Container_initialize();
+
+		if (!radius) radius = 40;
+		if (!color) color = "#F00";
+
+		this.radius = radius;
+		this.color = color;
+
+		var shape = new Shape();
+		shape.graphics.beginFill(color);
+		shape.graphics.drawCircle(0, 0, radius);
+		this.addChild(shape);
+	}
+
+	window.Ball = Ball;
+
+}(window));
